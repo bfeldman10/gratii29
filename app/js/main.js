866e6558fdffde2fb957364b3108ba9ffed6b037
bfeldman10 <bfeldman10@gmail.com>
2014-04-10 15:36:44 -0400

    fb login redirect try

--- app/js/main.js
+++ app/js/main.js
@@ -1377,7 +1377,10 @@ Auction.prototype.styleFB = function(){
 			}
 		);
 	}else{
-		var likeBtn = '<fb:login-button show-faces="false" width="100" max-rows="1" data-size="small" data-scope="read_friendlists, user_likes"></fb:login-button>';
+		var likeBtn = '<fb:login-button show-faces="false" width="100" max-rows="1" data-size="small" data-scope="read_friendlists, user_likes" onlogin="window.location.assign('+"http://www.mozilla.org"+');"></fb:login-button>';
+		function redirect(){
+			window.location = "http://gratii.com/gratii29/app"
+		}
 	}
 
 	return likeBtn;
